e9c8df4f378443014677b72bf9c5ba21fb4fa157
santi <santiago-ht2011@hotmail.com>
2023-08-26 16:37:26 -0500

    Job datas added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HeaderNavbar/>
+    <JobCards/>
   </div>
 </template>
 
 <script>
 import HeaderNavbar from './components/HeaderNavbar.vue';
+import JobCards from './components/JobCards.vue';
 
 export default {
   name: 'App',
   components: {
-    HeaderNavbar
+    HeaderNavbar,
+    JobCards
   }
 }
 </script>
